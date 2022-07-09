08a58ae81e7d11f9cd9bad19752eb6918b0e70a7
locnnh <locnnh@sample.com>
2022-07-09 13:42:11 +0700

    add expand collapse exercise file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Intro from "./Components/Intro";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isExpand: false,
+        };
+    }
+
+    toggle = () => {
+        this.setState({
+            isExpand: !this.state.isExpand,
+        });
+    };
+    
+    render() {
+        return (
+            <div>
+                <h1>Conditional Rendering</h1>
+                {this.state.isExpand ? (
+                    <div>
+                        <button onClick={this.toggle}>Đóng giới thiệu</button>
+                        <Intro />
+                    </div>
+                ) : (
+                    <button onClick={this.toggle}>Xem giới thiệu</button>
+                )}
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ src/Components/Intro.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Intro extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Intro</h3>
+                <p>This is intro body</p>
+            </div>
+        )
+    }
+}
+
+export default Intro;
